4a189ef15afc771d53dd33928ebf74855970b020
cx <553746088@qq.com>
2021-06-18 22:10:10 +0800

    2021 06 18 成功

---
+++ src/main/java/org/example/HttpClient.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import java.io.IOException;
+import java.util.Date;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * @author cx
+ * @create 2021-06-18 19:34
+ */
+public class HttpClient {
+    public static Log log = LogFactory.getLog(HttpClient.class);
+
+    public static void main(String[] args) throws Exception {
+
+        for (int i = 0; i < 10; i++) {
+            postHttpClient(i+1);
+
+
+        }
+    }
+    public static void postHttpClient(int threadID){
+        Thread readThread = new Thread(){
+            public void run(){
+                try {
+                    Date date = new Date();
+                    long startTime = date.getTime();
+                    String s = String.valueOf(startTime);
+                    String string=HttpClientUtils.postParameters("http://localhost:8888",s);
+                    long endTime = Long.parseLong(string.trim());
+                    if((endTime-startTime)<(threadID*1000)){
+                        String result="第"+threadID+"个请求在规定的时间响应成功";
+                        System.out.println(result);
+                        log.info(result);
+//                        log.error(result);
+//                        log.debug(result);
+                    }
+                    else{
+                        String result="第"+threadID+"个请求响应超时";
+                        System.out.println(result);
+                        log.info(result);
+//                        log.error(result);
+//                        log.debug(result);
+                    }
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+        readThread.start();
+    }
+}
---
+++ src/main/java/org/example/HttpServer.java
@@ -0,0 +1,79 @@
+package org.example;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+/**
+ * @author cx
+ * @create 2021-06-18 18:59
+ */
+public class HttpServer {
+    public static void main(String[] args) {
+        try {
+
+            /*监听端口号，只要是8888就能接收到*/
+            ServerSocket ss = new ServerSocket(8888);
+
+            while (true) {
+                /*实例化客户端，固定套路，通过服务端接受的对象，生成相应的客户端实例*/
+                Socket socket = ss.accept();
+                /*获取客户端输入流，就是请求过来的基本信息：请求头，换行符，请求体*/
+                BufferedReader bd = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                /**
+                 * 接受HTTP请求，并解析数据
+                 */
+                String requestHeader;
+                int contentLength = 0;
+                while ((requestHeader = bd.readLine()) != null && !requestHeader.isEmpty()) {
+                    System.out.println(requestHeader);
+                    /**
+                     * 获得GET参数
+                     */
+                    if (requestHeader.startsWith("GET")) {
+                        int begin = requestHeader.indexOf("/?") + 2;
+                        int end = requestHeader.indexOf("HTTP/");
+                        String condition = requestHeader.substring(begin, end);
+                        System.out.println("GET参数是：" + condition);
+                    }
+                    /**
+                     * 获得POST参数
+                     * 1.获取请求内容长度
+                     */
+                    if (requestHeader.startsWith("Content-Length")) {
+                        int begin = requestHeader.indexOf("Content-Lengh:") + "Content-Length:".length();
+                        String postParamterLength = requestHeader.substring(begin+1).trim();
+                        contentLength = Integer.parseInt(postParamterLength);
+                        System.out.println("POST参数长度是：" + Integer.parseInt(postParamterLength));
+                    }
+                }
+                StringBuffer sb = new StringBuffer();
+                if (contentLength > 0) {
+                    for (int i = 0; i < contentLength; i++) {
+                        sb.append((char) bd.read());
+                    }
+                    System.out.println("POST参数是：" + sb.toString());
+                }
+                /*发送回执*/
+                PrintWriter pw = new PrintWriter(socket.getOutputStream());
+                Date date=new Date();
+                long time = date.getTime();
+                String s = String.valueOf(time);
+                pw.println("HTTP/1.1 200 OK");
+                pw.println("Content-type:text/html");
+                pw.println();
+                System.out.println(s);
+                pw.println(s);
+                pw.flush();
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
